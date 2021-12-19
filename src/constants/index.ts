b55e479b56a3da8210305b9b11784f7e9bfe6fcd
wataruhigasi <ayumu_h130424@outlook.jp>
2021-12-19 19:13:53 +0900

    create-isuue

---
+++ src/constants/index.ts
@@ -0,0 +1,19 @@
+interface InputObject {
+    name: string,
+    romaji: string,
+}
+
+const characterData: InputObject[] = [
+    {
+        name: 'アスナ',
+        romaji: 'asuna',
+    },
+    {
+        name: 'キリト',
+        romaji: 'kirito',
+    }
+]
+
+export {
+    characterData
+}
