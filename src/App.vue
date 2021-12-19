04ec67b8a328f1c55e9737f06f4b5d55a5f61119
wataruhigasi <ayumu_h130424@outlook.jp>
2021-12-19 19:35:11 +0900

    toppage

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <Header />
+    <main class="py-12 max-w-screen-md mx-auto grid place-items-center">
+    </main>
+    <Footer />
   </div>
-  <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Footer from '@/components/Footer.vue'
+import Header from './components/Header.vue'
+  export default defineComponent({
+  name: 'App',
+  components: { Footer, Header },
+  })
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style scoped>
+main {
+  height: calc(100vh - 90px);
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="grid place-items-center bg-red-500 text-white text-center">
+        <small>&copy;2021 k-logic24</small>
+    </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+footer {
+    height: 40px;
+}
+</style>
